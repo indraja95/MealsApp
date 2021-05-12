ad28bf67405b50a1952dbb1c481f26997b06cd16
indraja <indraja.nutalapati@gmail.com>
2021-05-11 21:55:12 -0400

    Added components and styling to the project

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import React, {Fragment} from 'react';
+
+import MealSummary from './MealSummary';
+import AvailableMeal from './AvailableMeal';
+
+const Meals = () => {
+    return <Fragment>
+        <MealSummary />
+        <AvailableMeal />
+    </Fragment>;
+}
+
+export default Meals;
